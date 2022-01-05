c5f969d01d974013ebb00f4d86c54beb503e705d
KazMizuma <kaz87mizuma@gmail.com>
2022-01-05 15:41:45 -0500

    Raycasting added to detect nearby cars
    Detecting a car at 3 meters ahead

---
+++ Assets/Raycasting.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycasting : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Ray hittingRay = new Ray(gameObject.transform.position, gameObject.transform.forward);
+
+        Debug.DrawRay(gameObject.transform.position, gameObject.transform.forward * 3f, Color.green);
+
+        if (Physics.Raycast(hittingRay, out hit, 3f))
+        {
+            if (hit.transform.gameObject.tag == "Car")
+            {
+                Debug.Log("HIT THE " + hit.transform.gameObject.name + "!!!");
+            }
+        }
+    }
+}
