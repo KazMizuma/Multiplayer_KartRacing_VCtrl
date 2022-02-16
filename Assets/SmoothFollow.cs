092892c28e3a930447a7b0c7a2cd76a645be59a6
KazMizuma <kaz87mizuma@gmail.com>
2022-02-16 05:36:58 -0500

    Car Prefabs No Cams, 1st & 3rd Person Cam POV
    Main Camera was removed from Car Prefabs, 1st person camera POV is added to SmoothFollow script which is added to the Main Camera.

--- Assets/SmoothFollow.cs
+++ Assets/SmoothFollow.cs
@@ -10,10 +10,15 @@ public class SmoothFollow : MonoBehaviour
     public float heightOffset = 1.0f;
     public float heightDamping = 4.0f;
     public float rotationDamping = 2.0f;
+
+    int firstPersonPOV = -1; // when +1 first player, -1 third player pov
    
     void Start()
     {
-        
+        if (PlayerPrefs.HasKey("firstPersonPOV"))
+        {
+            firstPersonPOV = PlayerPrefs.GetInt("firstPersonPOV");
+        }
     }
 
     // Update is called once per frame
@@ -21,25 +26,42 @@ public class SmoothFollow : MonoBehaviour
     {
         if (target == null)
             return;
-            
-        float wantedRotationAngle = target.eulerAngles.y;
-        float wantedHeight = target.position.y + height;
 
-        float currentRotationAngle = transform.eulerAngles.y;
-        float currentHeight = transform.position.y;
+        if (firstPersonPOV == 1)
+        {
+            transform.position = target.position - target.forward * 0.85f + target.up * 1.25f;
+            transform.LookAt(target.position + target.forward * 3);
+        }
+        else
+        {
+            float wantedRotationAngle = target.eulerAngles.y;
+            float wantedHeight = target.position.y + height;
 
-        currentRotationAngle = Mathf.LerpAngle(currentRotationAngle, wantedRotationAngle, rotationDamping * Time.deltaTime);
-        currentHeight = Mathf.Lerp(currentHeight, wantedHeight, heightDamping * Time.deltaTime);
+            float currentRotationAngle = transform.eulerAngles.y;
+            float currentHeight = transform.position.y;
 
-        Quaternion currentRotation = Quaternion.Euler(0, currentRotationAngle, 0);
+            currentRotationAngle = Mathf.LerpAngle(currentRotationAngle, wantedRotationAngle, rotationDamping * Time.deltaTime);
+            currentHeight = Mathf.Lerp(currentHeight, wantedHeight, heightDamping * Time.deltaTime);
 
-        transform.position = target.position;
-        transform.position -= currentRotation * Vector3.forward * distance;
+            Quaternion currentRotation = Quaternion.Euler(0, currentRotationAngle, 0);
 
-        transform.position = new Vector3(transform.position.x,
-                                currentHeight + heightOffset,
-                                transform.position.z);
+            transform.position = target.position;
+            transform.position -= currentRotation * Vector3.forward * distance;
 
-        transform.LookAt(target);
+            transform.position = new Vector3(transform.position.x,
+                                    currentHeight + heightOffset,
+                                    transform.position.z);
+
+            transform.LookAt(target);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            firstPersonPOV = firstPersonPOV * -1;
+            PlayerPrefs.SetInt("firstPersonPOV", firstPersonPOV);
+        }
     }
 }
