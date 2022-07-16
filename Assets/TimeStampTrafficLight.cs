b8f5b220523a734a0c402223b553fff38b840259
KazMizuma <kaz87mizuma@gmail.com>
2022-07-16 10:11:50 -0400

    Traffic Control V5.1
    The traffic lights setup is complete. No traffic rules implemented.

--- Assets/TimeStampTrafficLight.cs
+++ Assets/TimeStampTrafficLight.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 
 public class TimeStampTrafficLight : MonoBehaviour
 {
-    public float timeAt6; // 6/30 Trfc Ctrl
-    public GameObject Light0; // 7/09 Trfc Ctrl, North
+    public float timeAt6; // 7/09 Trfc Ctrl, North
+    public GameObject Light0;  
     public float timeAt18; // South
     public GameObject Light2;
     public float timeAt36; // East
     public GameObject Light3;
     public float timeAt46; // West
     public GameObject Light1;
+
+    public float trafficLightTime = 0.0f;
+    //public float trafficLightTime2 = 0.0f;
+
+    //private string green;
+    //private string yellow;
+    //private string red;
 
     //AI_Controller ai_Controller;
 
@@ -21,45 +28,234 @@ public class TimeStampTrafficLight : MonoBehaviour
         //Light0.GetComponent<MeshRenderer>().materials[0].color = Color.gray; // 7/09 Trfc Ctrl
         //Debug.Log("trafficLight " + Light0.GetComponent<MeshRenderer>().materials[0].color);
 
-        timeAt6 = 0f; // North
-        timeAt18 = 0f; // South
-        timeAt36 = 0f; // East
-        timeAt46 = 0f; // West
+        timeAt6 = 0f; // Light0; North
+        timeAt18 = 0f; // Light2; South
+        timeAt36 = 0f; // Light3; East
+        timeAt46 = 0f; // Light1; West
 
         //Light0 = GameObject.Find("Light0Yellow");
         //Light0.GetComponent<Light>().enabled = false;
         //Light0 = GameObject.Find("Light0Red");
         //Light0.GetComponent<Light>().enabled = false;
-        Light0 = GameObject.Find("Light0Green");
-        Light0.GetComponent<Light>().enabled = true;
-        Debug.Log("Light0Green: " + Light0.GetComponent<Light>().enabled);
+        //Light0 = GameObject.Find("Light0Green");
+        //Light0.GetComponent<Light>().enabled = true;
+        //Debug.Log("Light0Green: " + Light0.GetComponent<Light>().enabled);
+
         //Light2 = GameObject.Find("Light2Yellow");
         //Light2.GetComponent<Light>().enabled = false;
         //Light2 = GameObject.Find("Light2Red");
         //Light2.GetComponent<Light>().enabled = false;
-        Light2 = GameObject.Find("Light2Green");
-        Light2.GetComponent<Light>().enabled = true;
-        Debug.Log("Light2Green: " + Light2.GetComponent<Light>().enabled);
+        //Light2 = GameObject.Find("Light2Green");
+        //Light2.GetComponent<Light>().enabled = true;
+        //Debug.Log("Light2Green: " + Light2.GetComponent<Light>().enabled);
+
         //Light3 = GameObject.Find("Light3Yellow");
         //Light3.GetComponent<Light>().enabled = false;
-        Light3 = GameObject.Find("Light3Red");
-        Light3.GetComponent<Light>().enabled = true;
+        //Light3 = GameObject.Find("Light3Red");
+        //Light3.GetComponent<Light>().enabled = true;
         //Light3 = GameObject.Find("Light3Green");
         //Light3.GetComponent<Light>().enabled = false;
-        Debug.Log("Light3Red: " + Light3.GetComponent<Light>().enabled);
+        //Debug.Log("Light3Red: " + Light3.GetComponent<Light>().enabled);
+
         //Light1 = GameObject.Find("Light1Yellow");
         //Light1.GetComponent<Light>().enabled = false;
-        Light1 = GameObject.Find("Light1Red");
-        Light1.GetComponent<Light>().enabled = true;
+        //Light1 = GameObject.Find("Light1Red");
+        //Light1.GetComponent<Light>().enabled = true;
         //Light1 = GameObject.Find("Light1Green");
         //Light1.GetComponent<Light>().enabled = false;
-        Debug.Log("Light1Red: " + Light1.GetComponent<Light>().enabled);
+        //Debug.Log("Light1Red: " + Light1.GetComponent<Light>().enabled);
+
+        //InvokeRepeating("At6At18Green", 20f, 20f);
+        //InvokeRepeating("At36At46Green", 20f, 20f);
+        //InvokeRepeating("At6At18Yellow", 2f, 40f);
+        //InvokeRepeating("At36At46Yellow", 2f, 40f);
+        //InvokeRepeating("At6At18Red", 20f, 40f);       
+        //InvokeRepeating("At36At46Red", 20f, 40f);
+    }
+
+    public void At6At18Green()
+    {
+        //Debug.Log("At6At18Green()");
+        Light0 = GameObject.Find("Light0Red");
+        Light0.GetComponent<Light>().enabled = false;
+        Light2 = GameObject.Find("Light2Red");
+        Light2.GetComponent<Light>().enabled = false;
+
+        Light0 = GameObject.Find("Light0Green");
+        Light0.GetComponent<Light>().enabled = true;
+        Light2 = GameObject.Find("Light2Green");
+        Light2.GetComponent<Light>().enabled = true;
+    }
+
+    public void At6At18Yellow()
+    {
+        //Debug.Log("At6At18Yellow()");
+        Light0 = GameObject.Find("Light0Green");
+        Light0.GetComponent<Light>().enabled = false;
+        Light2 = GameObject.Find("Light2Green");
+        Light2.GetComponent<Light>().enabled = false;
+
+        Light0 = GameObject.Find("Light0Yellow");
+        Light0.GetComponent<Light>().enabled = true;
+        Light2 = GameObject.Find("Light2Yellow");
+        Light2.GetComponent<Light>().enabled = true;
+    }
+
+    public void At6At18Red()
+    {
+        //Debug.Log("At6At18Red()");
+        Light0 = GameObject.Find("Light0Yellow");
+        Light0.GetComponent<Light>().enabled = false;
+        Light2 = GameObject.Find("Light2Yellow");
+        Light2.GetComponent<Light>().enabled = false;
+
+        Light0 = GameObject.Find("Light0Red");
+        Light0.GetComponent<Light>().enabled = true;
+        Light2 = GameObject.Find("Light2Red");
+        Light2.GetComponent<Light>().enabled = true;
+    }
+
+    public void At36At46Green()
+    {
+        //Debug.Log("At36At46Green()");
+        Light3 = GameObject.Find("Light3Red");
+        Light3.GetComponent<Light>().enabled = false;
+        Light1 = GameObject.Find("Light1Red");
+        Light1.GetComponent<Light>().enabled = false;
+
+        Light3 = GameObject.Find("Light3Green");
+        Light3.GetComponent<Light>().enabled = true;
+        Light1 = GameObject.Find("Light1Green");
+        Light1.GetComponent<Light>().enabled = true;
+    }
+
+    public void At36At46Yellow()
+    {
+        //Debug.Log("At36At46Yellow()");
+        Light3 = GameObject.Find("Light3Green");
+        Light3.GetComponent<Light>().enabled = false;
+        Light1 = GameObject.Find("Light1Green");
+        Light1.GetComponent<Light>().enabled = false;
+
+        Light3 = GameObject.Find("Light3Yellow");
+        Light3.GetComponent<Light>().enabled = true;
+        Light1 = GameObject.Find("Light1Yellow");
+        Light1.GetComponent<Light>().enabled = true;
+    }
+
+    public void At36At46Red()
+    {
+        //Debug.Log("At36At46Red()");
+        Light3 = GameObject.Find("Light3Yellow");
+        Light3.GetComponent<Light>().enabled = false;
+        Light1 = GameObject.Find("Light1Yellow");
+        Light1.GetComponent<Light>().enabled = false;
+
+        Light3 = GameObject.Find("Light3Red");
+        Light3.GetComponent<Light>().enabled = true;
+        Light1 = GameObject.Find("Light1Red");
+        Light1.GetComponent<Light>().enabled = true;
+    }
+
+    public void trafficLightAt6At18(string light)
+    {
+        switch (light)
+        {
+            case ("green"):
+                At6At18Green();
+                //Debug.Log("case (green): At6At18Green()");
+                //yield return new WaitForSecondsRealtime(20f);
+                break;
+
+            case ("yellow"):
+                At6At18Yellow();
+                //yield return new WaitForSecondsRealtime(2f);
+                break;
+
+            case ("red"):
+                At6At18Red();
+                //yield return new WaitForSecondsRealtime(20f);
+                break;
+
+            default:
+
+                break;
+        }
+    }
+
+    public void trafficLightAt36At46(string light)
+    {
+        switch (light)
+        {
+            case ("green"):
+                At36At46Green();
+                //yield return new WaitForSecondsRealtime(20f);
+                break;
+
+            case ("yellow"):
+                At36At46Yellow();
+                //yield return new WaitForSecondsRealtime(2f);
+                break;
+
+            case ("red"):
+                At36At46Red();
+                //Debug.Log("case (red): At36At46Red()");
+                //yield return new WaitForSecondsRealtime(20f);
+                break;
+
+            default:
+
+                break;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        trafficLightTime += Time.deltaTime;
+        if (trafficLightTime >= 0 && trafficLightTime < 20)
+        {
+            //Debug.Log("if (trafficLightTime0 >= 0)");
+            trafficLightAt6At18("green"); 
+        }
+        if (trafficLightTime >= 20 && trafficLightTime < 22)
+        {
+            trafficLightAt6At18("yellow");
+        }
+        if (trafficLightTime >= 22 && trafficLightTime < 42)
+        {
+            trafficLightAt6At18("red");
+        }
+        if (trafficLightTime >= 42)
+        {
+            trafficLightTime = 0;
+        }
 
+        //trafficLightTime2 += Time.deltaTime;
+        if (trafficLightTime >= 0 && trafficLightTime < 20)
+        {
+            //Debug.Log("if (trafficLightTime2 >= 0)");
+            trafficLightAt36At46("red");
+        }
+        if (trafficLightTime >= 20 && trafficLightTime < 40)
+        {
+            trafficLightAt36At46("green");
+        }
+        if (trafficLightTime >= 40 && trafficLightTime < 42)
+        {
+            trafficLightAt36At46("yellow");
+        }
+        //if (trafficLightTime >= 42)
+        //{
+        //    trafficLightTime2 = 0;
+        //}
+
+        //At6At18Green
+        //At36At46Green
+        //At6At18Yellow
+        //At36At46Yellow
+        //At6At18Red       
+        //At36At46Red
 
         //timeAt6 = ai_Controller.timeAt6;
         //timeAt18 = ai_Controller.timeAt18;
